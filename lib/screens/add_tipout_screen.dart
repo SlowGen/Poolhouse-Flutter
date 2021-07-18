36382acd8e93a1a94d6cbee311523eecd5b2efda
slowgen <kalibobailey@gmail.com>
2021-07-18 11:59:08 -0400

    Feat: refined styling on tipout screen

--- lib/screens/add_tipout_screen.dart
+++ lib/screens/add_tipout_screen.dart
@@ -16,34 +16,49 @@ class _AddTipoutScreenState extends State<AddTipoutScreen> {
     return Consumer<TipoutData>(builder: (context, tipoutData, child) {
       double tipoutValue = tipoutData.tipout.toDouble();
       return Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 25.0,
+        color: kPrimaryColorPeach,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+            height: 200.0,
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.0),
+              ),
             ),
-            Text('${tipoutValue.round()}% selected'),
-            Slider(
-              value: tipoutValue,
-              min: 0.0,
-              max: 50.0,
-              divisions: 50,
-              onChanged: (double tipout) {
-                setState(() {
-                  tipoutValue = tipout;
-                });
-                Provider.of<TipoutData>(context, listen: false)
-                    .setTipout(tipout.round());
-              },
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 25.0,
+                ),
+                Text('${tipoutValue.round()}% of total tips for tipout',
+                    style: kTextStyleInstructionsBody),
+                Slider(
+                  value: tipoutValue,
+                  min: 0.0,
+                  max: 50.0,
+                  divisions: 50,
+                  onChanged: (double tipout) {
+                    setState(() {
+                      tipoutValue = tipout;
+                    });
+                    Provider.of<TipoutData>(context, listen: false)
+                        .setTipout(tipout.round());
+                  },
+                ),
+                Text(
+                  'Select TOTAL tipout percentage here.',
+                  style: kTextStyleServerTileDelete,
+                ),
+                Text(
+                  'Use main app to divide tipouts after.',
+                  style: kTextStyleServerTileDelete,
+                ),
+              ],
             ),
-            Text(
-              'Select TOTAL tipout percentage here.',
-              style: kTextStyleServerTileDelete,
-            ),
-            Text(
-              'Use main app to divide tipouts after.',
-              style: kTextStyleServerTileDelete,
-            ),
-          ],
+          ),
         ),
       );
     });
--- lib/todos.dart
+++ lib/todos.dart
@@ -1,7 +1,7 @@
-//TODO: Figure out launcher icons for both platforms
 //TODO: Figure out styling for tip entry
 //TODO: Adjust splash screen animation time
 //TODO: Read through pre-release notes for both platforms
 
 //TODO: Research ad incorporation / payment details
 //TODO: Build out README and other documentation
+//TODO: Tweak Icon sizes before release
